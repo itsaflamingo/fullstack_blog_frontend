2d2e9ce443ab647972f53a1731d7b40adb07a4fa
Liz <elizabeth.pinero.r@gmail.com>
2023-04-06 14:40:47 -0400

    Archive year, month & day can now be dropped down

--- src/components/Aside/Archive.js
+++ src/components/Aside/Archive.js
@@ -8,33 +8,53 @@ export default function Archive() {
     const { data, loading } = useFetch({api: 'https://fs-blog-backend.fly.dev/blog', method: 'GET'})
     const [dates, setDates] = useState(null);
 
+    const [expandedYear, setExpandedYear] = useState(null);
+    const [expandedMonth, setExpandedMonth] = useState(null);
+    const [expandedDay, setExpandedDay] = useState(null);
+
     useEffect(() => {
         if(loading === true) return;
         setDates(createDatesDisplay(data));
     }, [loading])
 
-    return (
-        <div className="archive">
-            {dates && Object.keys(dates).map((year) => {
-                return <div key={uniqid()}>
-                <p>{year}</p>
-                {Object.keys(dates[year]).map((month) => (
-                  <div key={uniqid()}>
-                    <p>{month}</p>
-                      {Object.keys(dates[year][month]).map((day) => (
-                        <div key={uniqid()}>{day}
-                        {Object.values(dates[year][month][day]).map(blogPost => (
-                            <div key={uniqid()}>
-                                {blogPost}
-                                </div>
-                          ))}
-                          </div>
-                      ))}
+  return (
+    <div className="archive">
+      {dates && Object.keys(dates).map((year) => (
+        <div key={year}>
+          <div onClick={expandedYear ? () => setExpandedYear(null) : () => setExpandedYear(year)}>
+            {year} {expandedYear === year ? "▲" : "▼"}
+          </div>
+          {expandedYear === year && (
+            <>
+              {Object.keys(dates[year]).map((month) => (
+                <div key={`${year}-${month}`}>
+                  <div onClick={expandedMonth ? () => setExpandedMonth(null) : () => setExpandedMonth(month)}>
+                    {month} {expandedMonth === month ? "▲" : "▼"}
                   </div>
-                ))}
-              </div>
-          
-            })}
+                  {expandedMonth === month && (
+                    <>
+                      {Object.keys(dates[year][month]).map((day) => (
+                        <div key={uniqid()}>
+                          <div onClick={expandedDay ? () => setExpandedDay(null) : () => setExpandedDay(day)}>
+                            {day} {expandedDay === day ? "▲" : "▼"}
+                          </div>
+                          {expandedDay === day && (
+                            <ul>
+                              {dates[year][month][day].map((post) => (
+                                <li key={uniqid()}>{post}</li>
+                              ))}
+                            </ul>
+                          )}
+                        </div>
+                      ))}
+                    </>
+                  )}
+                </div>
+              ))}
+            </>
+          )}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
