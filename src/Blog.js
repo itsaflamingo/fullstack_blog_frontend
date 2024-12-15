e929bb353dafcd38ffb83fc4224aa6479d65abc4
Elizabeth Pinero <elizabeth.pinero.r@gmail.com>
2024-12-15 11:01:19 -0500

    updated aside, now top of page

--- src/Blog.js
+++ src/Blog.js
@@ -1,13 +1,12 @@
-import './App.css';
-import Aside from './components/Aside/Aside'
-import BlogPosts from './components/BlogPublic/BlogPosts';
+import "./App.css";
+import Aside from "./components/Aside/Aside";
+import BlogPosts from "./components/BlogPublic/BlogPosts";
 
 function Blog() {
-
   return (
     <div className="blog">
+      <Aside />
       <BlogPosts />
-      <Aside     />
     </div>
   );
 }
